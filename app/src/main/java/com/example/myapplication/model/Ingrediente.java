5545ba284eae18f64c974ebf4db0cc0b0a704f29
astreule <agustina.streule@rappi.com>
2022-06-23 16:00:47 -0300

    se agregan algunos cambios de la segunda pantalla de crear receta

--- app/src/main/java/com/example/myapplication/model/Ingrediente.java
+++ app/src/main/java/com/example/myapplication/model/Ingrediente.java
@@ -1,4 +1,37 @@
 package com.example.myapplication.model;
 
 public class Ingrediente {
+
+    private String nombre;
+    private Integer cantidad;
+    private String medida;
+
+    public Ingrediente(String nombre, Integer cantidad, String medida) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.medida = medida;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
 }
