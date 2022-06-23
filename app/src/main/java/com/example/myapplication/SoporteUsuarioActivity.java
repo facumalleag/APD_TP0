bf073bc3d30ef28084b89628e58da87d37fa464e
facum <facumalleag@gmail.com>
2022-06-23 01:54:47 -0300

    Login_Layout deleted, Activity Soporte usuario creada

---
+++ app/src/main/java/com/example/myapplication/SoporteUsuarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoporteUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soporte_usuario);
+    }
+}
